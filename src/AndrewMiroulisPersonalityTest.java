9750b47b2e13ff2d83682233ea53a0a339c9418f
amiroulis <amiroulis@student.mamkschools.org>
2018-11-20 09:56:22 -0500

    project sum

--- src/AndrewMiroulisPersonalityTest.java
+++ src/AndrewMiroulisPersonalityTest.java
@@ -1,11 +1,25 @@
+import java.io.File;
+import java.io.PrintStream;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 public class AndrewMiroulisPersonalityTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException  {
         Scanner input = new Scanner(System.in);
-        String inputs =  input.next();
-    //    while(inputs.exists())
+        Scanner scan = inputName(input);
+        PrintStream output = new PrintStream(input.next());
 
     }
+    public static Scanner inputName(Scanner input) throws FileNotFoundException {
+        System.out.print("Input File: ");
+        String name = input.next();
+        while (!(new File(name).exists())){
+            System.out.print("File name is invalid. Please try again.");
+            name = input.next();
+        }
+        Scanner scan = new Scanner(new File(name));
+        return scan;
+    }
+ //   public static void
 
 }
